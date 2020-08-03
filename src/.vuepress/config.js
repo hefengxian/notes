06583380dbebb4a57ccf38c18447c4f4b32dab79
Frank <jssmith883@gmail.com>
2020-08-03 21:23:36 +0800

    初始化架构、目录

---
+++ src/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    title: 'Notes',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.png' }]
+    ],
+    themeConfig: {
+        logo: '/assets/img/logo.svg',
+        nav: [
+            {text: '主页', link: '/'},
+            {
+                text: '笔记',
+                items: [
+                    {text: '开发笔记', link: '/note/dev/'},
+                    {text: '阅读笔记', link: '/note/read/'},
+                ],
+            },
+            {text: '计划', link: '/plan/'},
+            {text: '总结', link: '/review/'},
+            {text: '关于', link: '/about'},
+            {text: 'Github', link: '//github.com/hefengxian/', target: '_blank'},
+        ],
+    }
+}
---
+++ src/about.md
@@ -0,0 +1,5 @@
+
+I'm 𝓕𝓻𝓪𝓷𝓴.𝓗, here is where i place my notes in daily life.
+
+
+:tada: :heart:
